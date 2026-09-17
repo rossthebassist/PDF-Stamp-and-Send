e6557962cc3600dc686bc8c8465c143eb01c684f
rossthebassist <142048857+rossthebassist@users.noreply.github.com>
2026-09-17 23:03:58 +0100

    Add central app config for Zendesk custom field IDs and stamp defaults

---
+++ assets/app-config.js
@@ -0,0 +1,18 @@
+window.PDF_STAMP_CONFIG = {
+  appName: "PDF Stamp and Send",
+  version: "1.2",
+  customFieldIds: [
+    4850614726431,
+    5112618319903,
+    4922801016095,
+    4845512973727,
+    5361731296927,
+    5014173804959
+  ],
+  costCentreFieldId: 5361731296927,
+  driverNameFieldId: 5014173804959,
+  defaultStampSettings: {
+    fontSize: 10,
+    opacity: 0.85
+  }
+};
